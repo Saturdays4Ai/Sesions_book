6d1ea9d80c4351e27e0e018b9f5fe776a7aa1be7
Tebinski <daniel.gteba@gmail.com>
2021-01-30 16:07:11 +0100

    first commit

---
+++ Saturdays4Ai_book/_build/jupyter_execute/docs/Sesion2_1.py
@@ -0,0 +1,21 @@
+<h1>Table of Contents<span class="tocSkip"></span></h1>
+<div class="toc"><ul class="toc-item"></ul></div>
+
+# Hello world
+
+![](https://www.saturdays.ai/images/logo.png)
+
+print("Hola")
+
+print(Hola)
+
+Hola = "Hola" 
+
+print(Hola)
+
+a = 2
+
+print(a)
+
+print("Hola" + "Mundo" )
+
---
+++ Saturdays4Ai_book/_build/jupyter_execute/docs/Sesion2_2-Operadores.py
@@ -0,0 +1,51 @@
+<h1>Table of Contents<span class="tocSkip"></span></h1>
+<div class="toc"><ul class="toc-item"></ul></div>
+
+# Ejemplos operadores
+
+![](https://www.saturdays.ai/images/logo.png)
+
+a = 4
+b = 2
+
+a + b
+
+a - b
+
+a * b
+
+a / b
+
+c = 5
+
+c / b
+
+c // b
+
+c % b
+
+c
+
+
+b * (c//b) + (c%b)
+
+b
+
+2 * 2 * 2 * 2
+
+b ** a
+
+d = 0
+
+d = a + b
+
+d
+
+a += b
+
+a
+
+a -= b
+
+a
+
